429cf2c36868f1082d601046c8520e0b7d81a5ba
vkb0402-pixel <vkb0402@gmail.com>
2025-11-05 20:43:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+app.listen(PORT, () => console.log(`Server on ${PORT}`));
